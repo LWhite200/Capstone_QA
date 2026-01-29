5a0dc44afb506c2d09cb6bdf88bc5e4d209f6444
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2026-01-29 16:23:44 -0500

    Add files via upload

---
+++ PetPortal/ProgramFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetPortal
+{
+    internal class ProgramFile
+    {
+        public int DoublesNumber(int number)
+        {
+            return number * number;
+        }
+    }
+}
