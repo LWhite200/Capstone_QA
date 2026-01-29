06d126583e0d3ced335835f7203db29446fe61d6
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2026-01-29 15:40:12 -0500

    Add files via upload

---
+++ TEST_PetPortal/TEST_PetPortal.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetPortal;
+
+namespace PetPortal.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DoublesNumber_Input3_Returns9()
+        {
+            var portal = new PetPortal.PetPortal();
+            int result = portal.DoublesNumber(3);
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void DoublesNumber_Input5_Returns25()
+        {
+            var portal = new PetPortal.PetPortal();
+            int result = portal.DoublesNumber(5);
+            Assert.AreEqual(25, result);
+        }
+    }
+}
